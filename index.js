ee256281f97e01c1b77d3087d86c022e9f706ff1
rohankeenoy <103291728+rohankeenoy@users.noreply.github.com>
2022-11-04 20:04:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// values for seeding
+var shoe;
+var mode = document.getElementById("mode");
+var travel = document.getElementById("travel");
+
+
+function showValue(){
+    shoe = document.getElementById("shoe").value;
+    console.log(shoe);
+}
